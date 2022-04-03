b812cb83b34d38e093e26ca7a3b653f0ab5c8dec
eumosquera <endermu28@gmail.com>
2022-04-03 17:57:28 -0500

    CRUD- Para parcial Completo

--- University.Web/Controllers/InstructorsController.cs
+++ University.Web/Controllers/InstructorsController.cs
@@ -28,8 +28,122 @@ namespace University.Web.Controllers
             var instructorsDTO = instructorsModel.Select(x => mapper.Map<InstructorDTO>(x));
 
             return Json(instructorsDTO, JsonRequestBehavior.AllowGet);
+        } //INDEX
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+
+            return PartialView(new InstructorDTO());
         }
 
+        [HttpPost]
+        public async Task<ActionResult> Create(InstructorDTO instructorDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var instructorModel = mapper.Map<Instructor>(instructorDTO);
+                    await instructorRepository.Insert(instructorModel);
+
+                }
+
+                return Json(new ResponseDTO
+                {
+
+                    Message = "Process done successfull!",
+                    IsSuccess = true
+
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new ResponseDTO
+                {
+
+                    Message = ex.Message,
+                    IsSuccess = false
+
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Edit(int id)
+        {
+
+            var instructorModel = await instructorRepository.GetById(id);
+            var instructorDTO = mapper.Map<InstructorDTO>(instructorModel);
+
+            return PartialView(instructorDTO);
+        }//GET EDIT
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(InstructorDTO instructorDTO)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var instructorModel = mapper.Map<Instructor>(instructorDTO);
+                    await instructorRepository.Update(instructorModel);
+
+                }
+
+                return Json(new ResponseDTO
+                {
+
+                    Message = "Process done successfull!",
+                    IsSuccess = true
+
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new ResponseDTO
+                {
+
+                    Message = ex.Message,
+                    IsSuccess = false
+
+                }, JsonRequestBehavior.AllowGet);
+            }
+        }//POST 
+
+        [HttpGet]
+        public async Task<ActionResult> Delete(int id)
+        {
+
+            try
+            {
+                await instructorRepository.Delete(id);
+
+
+                return Json(new ResponseDTO
+                {
+
+                    Message = "Process done successfull!",
+                    IsSuccess = true
+
+                }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(new ResponseDTO
+                {
+
+                    Message = ex.Message,
+                    IsSuccess = false
+
+                }, JsonRequestBehavior.AllowGet);
+
+            }
+
+        }//DELETE
 
     }//CLAS
 }//NAMESPACE
